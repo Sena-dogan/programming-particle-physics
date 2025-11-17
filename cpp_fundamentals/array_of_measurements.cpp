d7c5cc8e06b816e2a05a7e14198e25f4698dbd2e
zeynep-sena-dogan <zeynepsena2002.d@gmail.com>
2025-11-17 17:03:41 +0300

    Array of Measurements

---
+++ cpp_fundamentals/array_of_measurements.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+int main()
+{
+    // Array of detector readings ( ADC counts )
+    int adcReadings[10] = {150, 145, 152, 148, 151,
+                           147, 149, 153, 146, 150};
+
+    /*
+     * // Low Standard Deviation: Data points are very close to the average (mean).
+     * // (e.g., [10, 10.1, 9.9, 10]) Your measurements are very consistent.
+     *
+     * // High Standard Deviation: Data points are spread out far from the average.
+     * // (e.g., [15, 5, 20, 0]) Your measurements are very inconsistent.
+     *
+     * --- Sample Standard Deviation Formula ---
+     * s = sqrt(  sum[ (x_i - x_bar)^2 ] / (n - 1)  )
+     */
+
+    // Calculate average
+    double sum = 0.0;
+    for (int i = 0; i < 10; i++)
+    {
+        sum += adcReadings[i];
+    }
+    double average = sum / 10.0;
+    // Calculate standard deviation
+    double variance = 0.0;
+    for (int i = 0; i < 10; i++)
+    {
+        // sum[ (x_i - x_bar)^2 ]
+        variance += (adcReadings[i] - average) *
+                    (adcReadings[i] - average);
+    }
+
+    // / (n - 1)
+    double stdDev = sqrt(variance / 9.0);
+    std ::cout << " Average : " << average << std ::endl;
+    std ::cout << " Std Dev : " << stdDev << std ::endl;
+    return 0;
+}
