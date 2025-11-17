da375296a3dcec3fe0ce26947534dd89426cd818
zeynep-sena-dogan <zeynepsena2002.d@gmail.com>
2025-11-17 17:06:31 +0300

    Array of Measurements

--- cpp_fundamentals/array_of_measurements.cpp
+++ cpp_fundamentals/array_of_measurements.cpp
@@ -6,16 +6,14 @@ int main()
     int adcReadings[10] = {150, 145, 152, 148, 151,
                            147, 149, 153, 146, 150};
 
-    /*
-     * // Low Standard Deviation: Data points are very close to the average (mean).
-     * // (e.g., [10, 10.1, 9.9, 10]) Your measurements are very consistent.
-     *
-     * // High Standard Deviation: Data points are spread out far from the average.
-     * // (e.g., [15, 5, 20, 0]) Your measurements are very inconsistent.
-     *
-     * --- Sample Standard Deviation Formula ---
-     * s = sqrt(  sum[ (x_i - x_bar)^2 ] / (n - 1)  )
-     */
+    //  // Low Standard Deviation: Data points are very close to the average (mean).
+    //  // (e.g., [10, 10.1, 9.9, 10]) Your measurements are very consistent.
+
+    //  // High Standard Deviation: Data points are spread out far from the average.
+    //  // (e.g., [15, 5, 20, 0]) Your measurements are very inconsistent.
+
+    //  --- Sample Standard Deviation Formula ---
+    //  s = sqrt(  sum[ (x_i - x_bar)^2 ] / (n - 1)  )
 
     // Calculate average
     double sum = 0.0;
